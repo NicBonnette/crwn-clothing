2293c9fe01649608c015f1b8c5c3c2d2971e96f7
Nic <nickypoo.is@gmail.com>
2020-10-04 15:09:59 +1300

    state of project after section 17 in course

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,23 +8,23 @@ import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles';
+import { CartDropdownContainer, CartItemsContainer, EmptyMessageContainer } from './cart-dropdown.styles';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
+    <CartDropdownContainer>
+        <CartItemsContainer>
             {cartItems.length ? (
                 cartItems.map(cartItem => (
                 <CartItem key={cartItem.id} item={cartItem} />
                 ))
             ) : (
-                <span className='empty-message'>
+                <EmptyMessageContainer>
                     Your cart is empty
-                </span>
+                </EmptyMessageContainer>
             )
             }
-
-        </div>
+        </CartItemsContainer>
         <CustomButton onClick={() => {
             history.push('/checkout');
             dispatch(toggleCartHidden());
@@ -32,7 +32,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         }>
             GO TO CHECKOUT
         </CustomButton>
-    </div>
+    </CartDropdownContainer>
 );
 
 const mapStateToProps =  createStructuredSelector({
